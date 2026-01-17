5a7a9dc6e7462642b52925fe2c2d5402131fa78e
WORK <jacobkiller1235@proton.me>
2026-01-17 16:42:22 +0500

    last

--- resources/views/pages/main/stats-test.blade.php
+++ resources/views/pages/main/stats-test.blade.php
@@ -4,6 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script>
+    // Добавляет header ко всем fetch-запросам на странице
+    (function() {
+      const originalFetch = window.fetch;
+      window.fetch = function(resource, config = {}) {
+        config.headers = config.headers || {};
+        // Добавьте нужный header, например X-Custom-Header
+        config.headers['X-My-Header'] = 'MyHeaderValue';
+        return originalFetch(resource, config);
+      };
+    })();
+    </script>
 </head>
 <body>
 
