1a9252fec57678e6b52b9f98999622a268256b91
WORK <jacobkiller1235@proton.me>
2026-03-16 22:31:01 +0500

    last

--- database/migrations/2026_02_05_171000_add_index_to_donates_steam_id.php
+++ database/migrations/2026_02_05_171000_add_index_to_donates_steam_id.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddIndexToDonateSteamId extends Migration
+class AddIndexToDonatesSteamId20260205 extends Migration
 {
     /**
      * Run the migrations.
